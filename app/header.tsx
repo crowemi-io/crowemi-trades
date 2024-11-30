3aab5c336de79349520f53c699cbd09c38dfdd9d
Andy Crowe <crowemi@hotmail.com>
2024-11-29 21:02:30 -0800

    update dockerfile

--- app/header.tsx
+++ app/header.tsx
@@ -3,7 +3,7 @@ export default function Header() {
       <div className="bg-white px-6 py-24 sm:py-32 lg:px-8">
         <div className="mx-auto max-w-2xl text-center">
           <h2 className="mt-2 text-5xl font-semibold tracking-tight text-gray-900 sm:text-7xl"><a href="#">crowemi-trades</a></h2>
-          <p className="text-base/7 font-semibold text-green-700">a simple speculative trading app</p>
+          <p className="text-base/7 font-semibold text-green-700">a simple speculative trading app w/REIT monthly dividend reinvestment.</p>
         </div>
       </div>
     )
