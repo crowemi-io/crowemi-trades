6d776a98eea65d647d1bbeaf5b2f863e05db246b
Andy Crowe <crowemi@hotmail.com>
2026-03-17 20:17:38 -0700

    update min/max

--- infra/main.tf
+++ infra/main.tf
@@ -10,6 +10,13 @@ resource "google_cloud_run_v2_service" "this" {
   ingress              = "INGRESS_TRAFFIC_INTERNAL_ONLY"
   launch_stage         = "BETA"
   default_uri_disabled = true
+
+  # Service-level min instances (template.scaling does not control min in v2 API)
+  scaling {
+    min_instance_count = 1
+    max_instance_count = 1
+  }
+
   template {
     containers {
       image = "us-west1-docker.pkg.dev/${var.gcp_project_id}/${var.gcp_project_id}/${local.service}:${var.docker_image_tag}"
@@ -18,7 +25,7 @@ resource "google_cloud_run_v2_service" "this" {
         value_source {
           secret_key_ref {
             secret  = data.google_secret_manager_secret.this.secret_id
-            version = "latest"
+            version = 5
           }
         }
       }
@@ -30,10 +37,6 @@ resource "google_cloud_run_v2_service" "this" {
         subnetwork = "projects/crowemi-io-${var.env}/regions/${var.gcp_region}/subnetworks/crowemi-io-${var.env}-subnet-01" # TODO: ref data
       }
       egress = "ALL_TRAFFIC"
-    }
-    scaling {
-      min_instance_count = 1
-      max_instance_count = 1
     }
   }
 }
